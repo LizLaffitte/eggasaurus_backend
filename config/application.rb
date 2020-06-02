3fc0e44f82677c53552fde2db5bc6c19452ed1f8
LizLaffitte <liz.kossakowski@gmail.com>
2020-06-02 17:51:31 -0400

    Changed where CORS configs were set.

--- config/application.rb
+++ config/application.rb
@@ -34,12 +34,7 @@ module EggasaurusBackend
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-          origins '*'
-          resource '*', headers: :any, methods: [:get, :post]
-      end
-    end
 
+    
   end
 end
