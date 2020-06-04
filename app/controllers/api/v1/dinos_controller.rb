16f9eb92cbd09afd3e89be19173dc599a4e9177a
LizLaffitte <liz.kossakowski@gmail.com>
2020-06-04 18:20:11 -0400

    added show and update routes and controllers.

--- app/controllers/api/v1/dinos_controller.rb
+++ app/controllers/api/v1/dinos_controller.rb
@@ -1,9 +1,24 @@
 class Api::V1::DinosController < ApplicationController
+    before_action :find_dino, only: [:show, :update, :destroy]
+
     def index
         dinos = Dino.all
         render json: DinoSerializer.new(dinos)
     end
 
+    def show
+        dino = Dino.find_by_id(params[:id])
+        render json: DinoSerializer.new(dino)
+    end
+
+    def update
+        @dino.update(dino_params)
+        if @dino.save
+          render json: @dino, status: :accepted
+        else
+          render json: { errors: @dino.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
     # def create
     #     dino = Dino.new(dino_params)
     #     if dino.save
@@ -14,7 +29,11 @@ class Api::V1::DinosController < ApplicationController
     private
     
     def dino_params
-        params.require(:dino).permit(:name, :happiness, :hunger, :tiredness, :category_id)
+        params.require(:dino).permit(:happiness, :hunger)
+    end
+
+    def find_dino
+        @dino = Dino.find_by_id(params[:id])
     end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :dinos, only: [:index]
+      resources :dinos, only: [:index, :show, :update]
     end
   end
 end
