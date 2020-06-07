ec780acf3b0094044bfdbae2b31aff6e0c34afa8
LizLaffitte <liz.kossakowski@gmail.com>
2020-06-06 20:22:04 -0400

    Changed foreign_key col name in Dinos table.

--- db/schema.rb
+++ db/schema.rb
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 2020_06_07_000837) do
     t.integer "tiredness"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "users_id"
+    t.integer "user_id"
     t.index ["specie_id"], name: "index_dinos_on_specie_id"
-    t.index ["users_id"], name: "index_dinos_on_users_id"
+    t.index ["user_id"], name: "index_dinos_on_user_id"
   end
 
   create_table "species", force: :cascade do |t|
@@ -41,5 +41,5 @@ ActiveRecord::Schema.define(version: 2020_06_07_000837) do
   end
 
   add_foreign_key "dinos", "species", column: "specie_id"
-  add_foreign_key "dinos", "users", column: "users_id"
+  add_foreign_key "dinos", "users"
 end
