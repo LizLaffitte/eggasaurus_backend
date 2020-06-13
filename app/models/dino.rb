75e0d0895f7beae821ae939e93c6f50a6ad8184c
LizLaffitte <liz.kossakowski@gmail.com>
2020-06-13 10:29:04 -0400

    Added model validations to Dino and Specie.

--- app/models/dino.rb
+++ app/models/dino.rb
@@ -2,4 +2,6 @@ class Dino < ApplicationRecord
   belongs_to :specie
   belongs_to :user
   validates :name, presence: true, uniqueness: {scope: :user_id,  message: "is already one of your dino's names."}
+  validates :specie, presence: true
+  validates :user, presence: true
 end
--- app/models/specie.rb
+++ app/models/specie.rb
@@ -1,4 +1,5 @@
 class Specie < ApplicationRecord
     has_many :dinos
     has_many :users, through: :dinos
+    validates :name, presence: true
 end
